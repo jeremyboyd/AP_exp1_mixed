749dbc2124ec8621816ab1e25a2cf1386f03218f
Jeremy Boyd <kenyonboyd@gmail.com>
2015-07-13 23:25:56 -0700

    Add keywords, remove table of contents.

--- publish_wp.R
+++ publish_wp.R
@@ -3,9 +3,8 @@ library(knitr)
 opts_knit$set(upload.fun = imgur_upload, base.url = NULL)
 opts_chunk$set(fig.width = 11, fig.height = 5, cache = TRUE)
 
-# Load markdown and set options to include table of contents
+# Load markdown
 library(markdown)
-options(markdown.HTML.options =  c(markdownHTMLOptions(default = T), "toc"))
 
 # Load RWordPress and login to WordPress. The source for RWordPress and dependency XMLRPC had to be downloaded and compiled localy from http://www.omegahat.org.
 library(RWordPress)
@@ -13,6 +12,10 @@ options(WordpressLogin = c(jeremy = ',lK1kY#t~E8:MWVD'),
         WordpressURL = 'http://www.jeremyboyd.org/xmlrpc.php')
 
 # Convert and upload .Rmd file to WordPress.
-knit2wp("AP_logit_mixed.Rmd", title = "A logit mixed model of a-adjective production", publish = FALSE)
-
-# Look this up: how to specify a key image in knit2wp?
+knit2wp("AP_exp1_mixed.Rmd",
+        title = "A logit mixed model of a-adjective production",
+        categories = c("statistical modeling"),
+        mt_keywords = c("R", "lme4", "mixed effects models",
+                        "language development", "grammatical constraints",
+                        "a-adjectives"),
+        publish = FALSE)
